8623b7aa336f981a0abbc2b9ab38533a66b68b0c
Juanclbici <juanclbici@gmail.com.mx>
2026-04-18 16:15:29 -0400

    Revert "Tercer cambio"
    This reverts commit 565f8ed3d96ce0bd26a119fb112f57ac98c357a1.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -2,10 +2,6 @@
 # Aquí hay una nueva actualización por <Juanclbici>
 print("Hola")
 
-# Here is another update by <Juanclbici>
-print("Hasta luego")
-
-
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
